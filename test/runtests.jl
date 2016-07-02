1a03a0a7ff0f18096f3cd301e3a69fdcfa8161ea
Eric Davies <iamed2@gmail.com>
2016-07-02 13:12:49 -0500

    Test memory allocation

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,3 +26,36 @@ using Base.Test
         rmprocs(bottom)
     end
 end
+
+@testset "Allocation" begin
+    rand_size = 800000000  # sizeof(rand(10000, 10000))
+    gc()
+    main_size = Base.summarysize(Main)
+
+    top = myid()
+    bottom = addprocs(1)[1]
+    @everywhere using DeferredFutures
+
+    try
+        df = DeferredFuture(top)
+
+        remote_size = remotecall_fetch(bottom, df) do dfr
+            gc()
+            main_size = Base.summarysize(Main)
+            put!(dfr, rand(10000, 10000))
+            main_size
+        end
+
+        gc()
+        @test Base.summarysize(Main) < main_size + rand_size
+
+        remote_size_new = remotecall_fetch(bottom) do
+            gc()
+            Base.summarysize(Main)
+        end
+
+        @test remote_size_new >= remote_size + rand_size
+    finally
+        rmprocs(bottom)
+    end
+end
